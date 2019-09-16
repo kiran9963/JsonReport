5ee8485750a6c4c484adeacab4478f1f6d173364
kiran9963 <kranthikiran7386@gmail.com>
2019-09-16 11:05:28 +0530

    newcode

--- src/test/java/cucum/stepdefinition/TestRunner.java
+++ src/test/java/cucum/stepdefinition/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features",glue="cucum.stepdefinition",
-monochrome=true,dryRun=false,strict = true,plugin= {"json:target/cucumber-reports/cucumber.jason","rerun:src/test/resources/Features/failed.txt"})
+monochrome=true,dryRun=false,strict = true,plugin= {"json:target/cucumber-reports/cucumber.json","rerun:src/test/resources/Features/failed.txt"})
 public class TestRunner {  
 
 }
